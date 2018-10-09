9251d02b839f7c84005d3edcfd8490c5ef604781
Xavier Jiang <xavierj2216@hstat.org>
2018-10-09 16:50:39 +0000

    fix errors and add return argument

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(num1,num2)
+    return num1/num2
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
+def greeting(arg1,arg2)
 
 end
 
 def return_a_value
-    
+  return "Nice"  
 end
 
-def pizza_party
-    
+def pizza_party(arg="cheese")
+    return arg
 end
 
-def whisper
+def whisper(arg)
+    return arg.downcase!
     
 end
